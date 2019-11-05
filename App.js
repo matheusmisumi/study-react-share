766e084484e6999f4b3e67685e8bb7fc025052d6
Matheus <>
2019-11-04 23:59:54 -0300

    Primeiro commit usando a lib Share do react-native funcionando

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import {StyleSheet, TouchableHighlight, Text, View, Share} from 'react-native';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  ShareMessage = () => {
+    Share.share({
+      message: 'xD',
+    })
+      .then(result => console.log(result))
+      .catch(errorMsg => console.log(errorMsg));
+  };
+
+  render() {
+    console.log(Share);
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight style={styles.button} onPress={this.ShareMessage}>
+          <Text> Touch Here </Text>
+        </TouchableHighlight>
+        <View style={[styles.countContainer]}>
+          <Text style={[styles.countText]}>
+            {this.state.count !== 0 ? this.state.count : null}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+  countContainer: {
+    alignItems: 'center',
+    padding: 10,
+  },
+  countText: {
+    color: '#FF00FF',
+  },
+});
